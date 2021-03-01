3981777e6e4701781dcdb6ca19c1959356473373
tjs531 <tjs531@naver.com>
2021-03-01 20:54:53 +0900

    selMarker 배열에 저장하는 대신 하나씩 생성/제거. DB nullpointexception 해결해야함.

--- MyTravel/src/main/java/com/hs/mytravel/place/PlaceController.java
+++ MyTravel/src/main/java/com/hs/mytravel/place/PlaceController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hs.mytravel.place.model.PlaceVO;
 
 @Controller
 @RequestMapping("/place")
@@ -18,6 +21,12 @@ public class PlaceController {
 		model.addAttribute("title", "루트 만들기");
 		model.addAttribute("view", "/travel/travelMap");
 		return "home";
+	}
+	
+	@RequestMapping(value="/ajaxInsPlace", method = RequestMethod.POST)
+	@ResponseBody
+	public int ajaxInsPlace(PlaceVO vo) {
+		return service.insPlace(vo);
 	}
 	
 	
--- MyTravel/src/main/java/com/hs/mytravel/place/PlaceService.java
+++ MyTravel/src/main/java/com/hs/mytravel/place/PlaceService.java
@@ -1,8 +1,17 @@
 package com.hs.mytravel.place;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hs.mytravel.place.model.PlaceVO;
 
 @Service
 public class PlaceService {
+	
+	@Autowired
+	private PlaceMapper mapper;
 
+	public int insPlace(PlaceVO vo) {
+		return mapper.insPlace(vo);
+	}
 }
